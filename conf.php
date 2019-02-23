7b12d8c0209403dd0d834773487835688bc47261
P1n9o <king.medo7200@gmail.com>
2019-02-23 14:13:54 +0200

    add --now

--- conf.php
+++ conf.php
@@ -2,9 +2,9 @@
 
 $conf = [
 	//Change to your bot token, check the readme to know how to create and get your bot token.
-	'bot_token' => 'DDDDDDDDD:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+	'bot_token' => '770095762:AAHJtDQMTeWjQh_fFkZQw3gBxpZezBcANHQ',
 	//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
-	'only_trusted' => TRUE,
+	'only_trusted' => FALSE,
 	//Populate the array with the trusted Chat IDs
 	'trusted' => [ 
 		'<chat_id>' 
